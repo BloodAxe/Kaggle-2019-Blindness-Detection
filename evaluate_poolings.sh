f884b9940503ddc2f1879d18153834f84a083c2a
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-07-18 12:27:20 +0300

    Update maxavg pool

--- evaluate_poolings.sh
+++ evaluate_poolings.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
 
-python train_classifier_baseline.py -m cls_resnext50_gap    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#python train_classifier_baseline.py -m cls_resnext50_gap    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#sleep 5
+#
+#python train_classifier_baseline.py -m cls_resnext50_gmp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#sleep 5
+#
+#python train_classifier_baseline.py -m cls_resnext50_gwap   -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#sleep 5
+#
+#python train_classifier_baseline.py -m cls_resnext50_ocp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#sleep 5
+#
+#python train_classifier_baseline.py -m cls_resnext50_rms    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+#sleep 5
+
+python train_classifier_baseline.py -m cls_resnext50_maxavg -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-python train_classifier_baseline.py -m cls_resnext50_gmp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_gap    -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-python train_classifier_baseline.py -m cls_resnext50_gwap   -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_gmp    -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-python train_classifier_baseline.py -m cls_resnext50_ocp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_gwap   -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-python train_classifier_baseline.py -m cls_resnext50_rms    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_ocp    -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-python train_classifier_baseline.py -m cls_resnext50_maxavg -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_rms    -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
 
-
-python train_regression_baseline.py -m reg_resnext50_gap    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
-sleep 5
-
-python train_regression_baseline.py -m reg_resnext50_gmp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
-sleep 5
-
-python train_regression_baseline.py -m reg_resnext50_gwap   -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
-sleep 5
-
-python train_regression_baseline.py -m reg_resnext50_ocp    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
-sleep 5
-
-python train_regression_baseline.py -m reg_resnext50_rms    -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
-sleep 5
-
-python train_regression_baseline.py -m reg_resnext50_maxavg -b 60 -e 50 -es 20 -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
+python train_regression_baseline.py -m reg_resnext50_maxavg -b 60 -e 50 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fast --fp16 --balance
 sleep 5
