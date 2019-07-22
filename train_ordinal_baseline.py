5f40de6e4caaee1a9085ecc5e098e98f58705b4e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-07-22 18:16:45 +0300

    Update oridnal regression

--- train_ordinal_baseline.py
+++ train_ordinal_baseline.py
@@ -12,7 +12,7 @@ from catalyst.dl.callbacks import MixupCallback
 from catalyst.utils import load_checkpoint, unpack_checkpoint
 from pytorch_toolbelt.utils import fs
 from pytorch_toolbelt.utils.catalyst import ShowPolarBatchesCallback, ConfusionMatrixCallback
-from pytorch_toolbelt.utils.random import set_manual_seed
+from pytorch_toolbelt.utils.random import set_manual_seed, get_random_name
 from pytorch_toolbelt.utils.torch_utils import maybe_cuda, count_parameters, \
     set_trainable
 
@@ -22,6 +22,7 @@ from retinopathy.lib.dataset import get_class_names, \
 from retinopathy.lib.factory import get_model, get_loss, get_optimizer, \
     get_optimizable_parameters, get_scheduler
 from retinopathy.lib.visualization import draw_classification_predictions
+from retinopathy.scripts.clean_checkpoint import clean_checkpoint
 
 
 def main():
@@ -32,6 +33,10 @@ def main():
     parser.add_argument('--balance', action='store_true')
     parser.add_argument('--swa', action='store_true')
     parser.add_argument('--show', action='store_true')
+    parser.add_argument('--use-idrid', action='store_true')
+    parser.add_argument('--use-messidor', action='store_true')
+    parser.add_argument('--use-aptos2015', action='store_true')
+    parser.add_argument('--use-aptos2019', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-acc', '--accumulation-steps', type=int, default=1, help='Number of batches to process')
     parser.add_argument('-dd', '--data-dir', type=str, default='data', help='Data directory')
@@ -53,8 +58,12 @@ def main():
     parser.add_argument('--transfer', default=None, type=str, help='')
     parser.add_argument('--fp16', action='store_true')
     parser.add_argument('-s', '--scheduler', default='multistep', type=str, help='')
+    parser.add_argument('--size', default=512, type=int, help='Image size for training & inference')
     parser.add_argument('-wd', '--weight-decay', default=0, type=float, help='L2 weight decay')
+    parser.add_argument('--warmup', default=0, type=int,
+                        help='Number of warmup epochs with 0.1 of the initial LR and frozed encoder')
 
+    # '--use-messidor --use-aptos2019 --use-idrid'
     args = parser.parse_args()
 
     data_dir = args.data_dir
@@ -65,7 +74,7 @@ def main():
     early_stopping = args.early_stopping
     model_name = args.model
     optimizer_name = args.optimizer
-    image_size = (512, 512)
+    image_size = (args.size, args.size)
     fast = args.fast
     augmentations = args.augmentations
     fp16 = args.fp16
@@ -79,11 +88,29 @@ def main():
     scheduler_name = args.scheduler
     verbose = args.verbose
     weight_decay = args.weight_decay
+    use_idrid = args.use_idrid
+    use_messidor = args.use_messidor
+    use_aptos2015 = args.use_aptos2015
+    use_aptos2019 = args.use_aptos2019
+    warmup = args.warmup
+
+    assert use_aptos2015 or use_aptos2019 or use_idrid or use_messidor
+
+    current_time = datetime.now().strftime('%b%d_%H_%M')
 
     if folds is None or len(folds) == 0:
         folds = [None]
 
     for fold in folds:
+        checkpoint_prefix = f'{model_name}_{args.size}_{get_random_name()}_fold{fold}'
+        if use_aptos2019:
+            checkpoint_prefix += '_aptos2019'
+        if use_aptos2015:
+            checkpoint_prefix += '_aptos2015'
+        if use_messidor:
+            checkpoint_prefix += '_messidor'
+        if use_idrid:
+            checkpoint_prefix += '_idrid'
 
         set_manual_seed(args.seed)
         model = maybe_cuda(
@@ -124,37 +151,23 @@ def main():
                   checkpoint['epoch_metrics']['valid']['accuracy'],
                   'loss:', checkpoint['epoch_metrics']['valid']['loss'])
 
-        if freeze_encoder:
-            set_trainable(model.encoder, trainable=False, freeze_bn=True)
+        train_ds, valid_ds, train_sizes = get_datasets(data_dir=data_dir,
+                                                       use_aptos2019=use_aptos2019,
+                                                       use_aptos2015=use_aptos2015,
+                                                       use_idrid=use_idrid,
+                                                       use_messidor=use_messidor,
+                                                       image_size=image_size,
+                                                       augmentation=augmentations,
+                                                       target_dtype=int,
+                                                       fold=fold,
+                                                       folds=4)
 
-        criterion = get_loss(criterion_name)
-        parameters = get_optimizable_parameters(model)
-        optimizer = get_optimizer(optimizer_name, parameters,
-                                  learning_rate=learning_rate,
-                                  weight_decay=weight_decay)
-
-        if checkpoint is not None:
-            try:
-                unpack_checkpoint(checkpoint, optimizer=optimizer)
-                print('Restored optimizer state from checkpoint')
-            except Exception as e:
-                print('Failed to restore optimizer state from checkpoint', e)
-
-        train_ds, valid_ds = get_datasets(data_dir=data_dir,
-                                          use_aptos2019=True,
-                                          use_aptos2015=not fast,
-                                          use_idrid=not fast,
-                                          use_messidor=not fast,
-                                          image_size=image_size,
-                                          augmentation=augmentations,
-                                          target_dtype=int,
-                                          fold=fold,
-                                          folds=4)
-
+        not_using_extra_data = not (use_idrid and use_messidor and use_aptos2015)
         train_loader, valid_loader = get_dataloaders(train_ds, valid_ds,
                                                      batch_size=batch_size,
                                                      num_workers=num_workers,
-                                                     oversample_factor=2 if fast else 1,
+                                                     oversample_factor=2 if not_using_extra_data else 1,
+                                                     train_sizes=train_sizes,
                                                      balance=balance)
 
         if use_swa:
@@ -167,48 +180,40 @@ def main():
         loaders["train"] = train_loader
         loaders["valid"] = valid_loader
 
-        current_time = datetime.now().strftime('%b%d_%H_%M')
-        prefix = f'ordinal/{model_name}/fold_{fold}/{current_time}_{criterion_name}'
-
-        if fp16:
-            prefix += '_fp16'
-
-        if fast:
-            prefix += '_fast'
+        prefix = f'ordinal/{model_name}/{current_time}/{checkpoint_prefix}'
 
         log_dir = os.path.join('runs', prefix)
         os.makedirs(log_dir, exist_ok=False)
 
-        scheduler = get_scheduler(scheduler_name, optimizer,
-                                  lr=learning_rate,
-                                  num_epochs=num_epochs,
-                                  batches_in_epoch=len(train_loader))
-
+        print('Datasets         :', data_dir)
+        print('  Train size     :', len(train_loader), len(train_loader.dataset))
+        print('  Valid size     :', len(valid_loader), len(valid_loader.dataset))
+        print('  Aptos 2019     :', use_aptos2019)
+        print('  Aptos 2015     :', use_aptos2015)
+        print('  IDRID          :', use_idrid)
+        print('  Messidor       :', use_messidor)
         print('Train session    :', prefix)
-        print('\tFP16 mode      :', fp16)
-        print('\tFast mode      :', fast)
-        print('\tMixup          :', mixup)
-        print('\tBalance        :', balance)
-        print('\tEpochs         :', num_epochs)
-        print('\tEarly stopping :', early_stopping)
-        print('\tWorkers        :', num_workers)
-        print('\tData dir       :', data_dir)
-        print('\tFold           :', fold)
-        print('\tLog dir        :', log_dir)
-        print('\tAugmentations  :', augmentations)
-        print('\tTrain size     :', len(train_loader),
-              len(train_loader.dataset))
-        print('\tValid size     :', len(valid_loader),
-              len(valid_loader.dataset))
+        print('  FP16 mode      :', fp16)
+        print('  Fast mode      :', fast)
+        print('  Mixup          :', mixup)
+        print('  Balance        :', balance)
+        print('  Warmup epoch   :', warmup)
+        print('  Train epochs   :', num_epochs)
+        print('  Workers        :', num_workers)
+        print('  Fold           :', fold)
+        print('  Log dir        :', log_dir)
+        print('  Augmentations  :', augmentations)
         print('Model            :', model_name)
-        print('\tParameters     :', count_parameters(model))
-        print('\tImage size     :', image_size)
-        print('\tFreeze encoder :', freeze_encoder)
+        print('  Parameters     :', count_parameters(model))
+        print('  Image size     :', image_size)
+        print('  Freeze encoder :', freeze_encoder)
         print('Optimizer        :', optimizer_name)
-        print('\tLearning rate  :', learning_rate)
-        print('\tBatch size     :', batch_size)
-        print('\tCriterion      :', criterion_name)
-        print('\tScheduler      :', scheduler_name)
+        print('  Learning rate  :', learning_rate)
+        print('  Batch size     :', batch_size)
+        print('  Criterion      :', criterion_name)
+        print('  Scheduler      :', scheduler_name)
+        print('  Weight decay   :', weight_decay)
+        print('  Early stopping :', early_stopping)
 
         # model training
         visualization_fn = partial(draw_classification_predictions,
@@ -221,20 +226,66 @@ def main():
             AscensionCallback(model)
         ]
 
+        criterion = get_loss(criterion_name)
+        runner = SupervisedRunner(input_key='image')
+        # Pretrain/warmup
+        if warmup:
+            set_trainable(model.encoder, False, False)
+            optimizer = get_optimizer(optimizer_name, get_optimizable_parameters(model),
+                                      learning_rate=learning_rate * 0.1,
+                                      weight_decay=weight_decay)
+            runner.train(
+                fp16=fp16,
+                model=model,
+                criterion=criterion,
+                optimizer=optimizer,
+                scheduler=None,
+                callbacks=callbacks,
+                loaders=loaders,
+                logdir=os.path.join(log_dir, 'warmup'),
+                num_epochs=warmup,
+                verbose=verbose,
+                main_metric='kappa_score',
+                minimize_metric=False,
+                checkpoint_data={"cmd_args": vars(args)}
+            )
+
+            del optimizer
+
         if mixup:
             callbacks += [MixupCallback(fields=['image'])]
 
         if early_stopping:
             callbacks += [
-                EarlyStoppingCallback(early_stopping,
-                                      metric='kappa_score', minimize=False)]
+                EarlyStoppingCallback(early_stopping, metric='kappa_score',
+                                      minimize=False)]
 
         if show_batches:
             callbacks += [
-                ShowPolarBatchesCallback(visualization_fn,
-                                         metric='accuracy', minimize=False)]
+                ShowPolarBatchesCallback(visualization_fn, metric='accuracy',
+                                         minimize=False)]
 
-        runner = SupervisedRunner(input_key='image')
+        # Main train
+        set_trainable(model.encoder, True, False)
+        if freeze_encoder:
+            set_trainable(model.encoder, trainable=False, freeze_bn=False)
+
+        optimizer = get_optimizer(optimizer_name, get_optimizable_parameters(model),
+                                  learning_rate=learning_rate,
+                                  weight_decay=weight_decay)
+
+        scheduler = get_scheduler(scheduler_name, optimizer,
+                                  lr=learning_rate,
+                                  num_epochs=num_epochs,
+                                  batches_in_epoch=len(train_loader))
+
+        if checkpoint is not None:
+            try:
+                unpack_checkpoint(checkpoint, optimizer=optimizer)
+                print('Restored optimizer state from checkpoint')
+            except Exception as e:
+                print('Failed to restore optimizer state from checkpoint', e)
+
         runner.train(
             fp16=fp16,
             model=model,
@@ -253,28 +304,9 @@ def main():
 
         del runner, callbacks, loaders, optimizer, model, criterion, scheduler
 
-        # if fold is not None:
-        #     dataset_fname = os.path.join(data_dir, 'train_with_folds.csv')
-        #     dataset = pd.read_csv(dataset_fname)
-        #     oof_csv = dataset[dataset['fold'] == fold]
-        #
-        #     model_checkpoint = os.path.join(log_dir, 'checkpoints', 'best.pth')
-        #     oof_predictions = run_model_inference(
-        #         model_checkpoint=model_checkpoint,
-        #         test_csv=oof_csv,
-        #         images_dir='train_images',
-        #         data_dir=data_dir,
-        #         batch_size=batch_size,
-        #         tta=None,
-        #         apply_softmax=False)
-        #
-        #     checkpoint = load_checkpoint(model_checkpoint)
-        #     del checkpoint['criterion_state_dict']
-        #     del checkpoint['optimizer_state_dict']
-        #     del checkpoint['scheduler_state_dict']
-        #     checkpoint['oof_predictions'] = oof_predictions.to_dict()
-        #     torch.save(checkpoint, os.path.join(log_dir, 'checkpoints',
-        #                                         f'{model_name}_fold{fold}' + '.pth'))
+        best__checkpoint = os.path.join(log_dir, 'checkpoints', 'best.pth')
+        model_checkpoint = os.path.join(log_dir, 'checkpoints', f'{checkpoint_prefix}_best.pth')
+        clean_checkpoint(best__checkpoint, model_checkpoint)
 
 
 if __name__ == '__main__':
