3d04ef4f805b98636b90554b9426c44a2201e14b
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-07-22 09:27:35 +0300

    Fine-tune and added image-size as parameter

--- finetune_reg_resnext50_rms.sh
+++ finetune_reg_resnext50_rms.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-python train_regression_baseline.py -m reg_resnext50_rms -b 60 -e 100 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fp16 -f 0 --use-aptos2019 --use-idrid --use-messidor -c pretrained/791/reg_resnext50_rms/fold_0/Jul18_15_49_wing_loss_fp16_fast/checkpoints/reg_resnext50_rms_fold0_best.pth
+python train_regression_baseline.py -m reg_resnext50_rms --size 768 --fp16 -b 30 -e 100 -l wing_loss -lr 1e-4 -wd 1e-3 -o Adam -a hard -f 0 --use-aptos2019 --use-idrid --use-messidor -c pretrained/cls_resnext50_aptos2019_idrid_messidor/reg_resnext50_rms_eager_pike_fold0_aptos2019_messidor_idrid_best.pth
 sleep 5
 
-python train_regression_baseline.py -m reg_resnext50_rms -b 60 -e 100 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fp16 -f 1 --use-aptos2019 --use-idrid --use-messidor -c pretrained/791/reg_resnext50_rms/fold_1/Jul18_17_17_wing_loss_fp16_fast/checkpoints/reg_resnext50_rms_fold1_best.pth
+python train_regression_baseline.py -m reg_resnext50_rms --size 768 --fp16 -b 30 -e 100 -l wing_loss -lr 1e-4 -wd 1e-3 -o Adam -a hard -f 1 --use-aptos2019 --use-idrid --use-messidor -c pretrained/cls_resnext50_aptos2019_idrid_messidor/reg_resnext50_rms_512_affectionate_mayer_fold1_aptos2019_messidor_idrid_best.pth
 sleep 5
 
-python train_regression_baseline.py -m reg_resnext50_rms -b 60 -e 100 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fp16 -f 2 --use-aptos2019 --use-idrid --use-messidor -c pretrained/791/reg_resnext50_rms/fold_2/Jul18_18_45_wing_loss_fp16_fast/checkpoints/reg_resnext50_rms_fold2_best.pth
+python train_regression_baseline.py -m reg_resnext50_rms --size 768 --fp16 -b 30 -e 100 -l wing_loss -lr 1e-4 -wd 1e-3 -o Adam -a hard -f 2 --use-aptos2019 --use-idrid --use-messidor -c pretrained/cls_resnext50_aptos2019_idrid_messidor/reg_resnext50_rms_512_kind_wing_fold2_aptos2019_messidor_idrid_best.pth
 sleep 5
 
-python train_regression_baseline.py -m reg_resnext50_rms -b 60 -e 100 -es 20 -l wing_loss -lr 1e-4 -o Adam -a hard --fp16 -f 2 --use-aptos2019 --use-idrid --use-messidor -c pretrained/791/reg_resnext50_rms/fold_3/Jul18_20_14_wing_loss_fp16_fast/checkpoints/reg_resnext50_rms_fold3_best.pth
+python train_regression_baseline.py -m reg_resnext50_rms --size 768 --fp16 -b 30 -e 100 -l wing_loss -lr 1e-4 -wd 1e-3 -o Adam -a hard -f 3 --use-aptos2019 --use-idrid --use-messidor -c pretrained/cls_resnext50_aptos2019_idrid_messidor/reg_resnext50_rms_512_thirsty_noyce_fold3_aptos2019_messidor_idrid_best.pth
 sleep 5
