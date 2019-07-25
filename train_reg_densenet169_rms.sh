5866e3d336d5e09afeef42c0c9555f099cbefeb3
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-07-25 17:32:18 +0300

    Update scripts

--- train_reg_densenet169_rms.sh
+++ train_reg_densenet169_rms.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-python train_regression_baseline.py -m reg_densenet169_rms -a hard -f 0 -b 30 -l huber --fp16 -o Adam -d 0.5\
-    -s multistep -lr 1e-4 -e 100 -es 10 -v --use-idrid --use-messidor --use-aptos2019
+python train_regression_baseline.py -m reg_densenet169_rms -a hard -f 0 -f 1 -f 2 -f 3 -b 30 -l huber --fp16 -o Adam -d 0.5\
+    -s multistep -lr 1e-4 -e 100 -es 10 -v --use-idrid --use-messidor --use-aptos2019 --warmup 20
 sleep 5
-
-python train_regression_baseline.py -m reg_densenet169_rms -a hard -f 1 -b 30 -l huber --fp16 -o Adam -d 0.5\
-    -s multistep -lr 1e-4 -e 100 -es 10 -v --use-idrid --use-messidor --use-aptos2019
-sleep 5
-
-python train_regression_baseline.py -m reg_densenet169_rms -a hard -f 2 -b 30 -l huber --fp16 -o Adam -d 0.5\
-    -s multistep -lr 1e-4 -e 100 -es 10 -v --use-idrid --use-messidor --use-aptos2019
-sleep 5
-
-python train_regression_baseline.py -m reg_densenet169_rms -a hard -f 3 -b 30 -l huber --fp16 -o Adam -d 0.5\
-    -s multistep -lr 1e-4 -e 100 -es 10 -v --use-idrid --use-messidor --use-aptos2019
