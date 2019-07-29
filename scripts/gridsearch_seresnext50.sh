8cd8449e4c56981d4547a55055edd27d793a41fa
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-07-29 23:14:00 +0300

    Fix parameter

--- scripts/gridsearch_seresnext50.sh
+++ scripts/gridsearch_seresnext50.sh
@@ -2,26 +2,26 @@
 
 # Pretrain model to start with something more or less accurate
 python train_classifier_baseline.py -m cls_seresnext50_max -a light -f 0 -b 60 --fp16 -o Adam -lr 1e-4\
-  -e 20 -v --use-aptos2019 --use-idrid --use-messidor --warmup 20 --loss ce --transfer cls_seresnext50_max_pretrain.pth
+  -e 20 -v --use-aptos2019 --use-idrid --use-messidor --warmup 20 --criterion ce --transfer cls_seresnext50_max_pretrain.pth
 sleep 5
 
 
 # Let's check different loss functions
 # Loss function: soft_ce
 python train_classifier_baseline.py -m cls_seresnext50_max -a light -f 0 -b 60 --fp16 -o Adam -d 0.5 -lr 1e-4\
-  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --loss soft_ce --transfer cls_seresnext50_max_pretrain.pth
+  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --criterion soft_ce --transfer cls_seresnext50_max_pretrain.pth
 sleep 5
 
 # Let's check different loss functions
 # Loss function: hybrid_kappa
 python train_classifier_baseline.py -m cls_seresnext50_max -a light -f 0 -b 60 --fp16 -o Adam -d 0.5 -lr 1e-4\
-  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --loss hybrid_kappa --transfer cls_seresnext50_max_pretrain.pth
+  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --criterion hybrid_kappa --transfer cls_seresnext50_max_pretrain.pth
 sleep 5
 
 # Let's check different loss functions
 # Loss function: focal
 python train_classifier_baseline.py -m cls_seresnext50_max -a light -f 0 -b 60 --fp16 -o Adam -d 0.5 -lr 1e-4\
-  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --loss focal --transfer cls_seresnext50_max_pretrain.pth
+  -e 50 -v --use-aptos2019 --use-idrid --use-messidor --criterion focal --transfer cls_seresnext50_max_pretrain.pth
 sleep 5
 
 
