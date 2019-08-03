de6e538c31132216fcb7814206dbf3638968aa42
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-08-03 23:27:46 +0300

    Update models training scripts

--- scripts/gridsearch_seresnext50.cmd
+++ scripts/gridsearch_seresnext50.cmd
@@ -1,6 +1,28 @@
 
 REM Pretrain model to start with something more or less accurate
-python train_classifier_baseline.py -m seresnext50_max -a light -f 0 -b 60 --fp16 -o Adam -lr 1e-4\
-  -e 20 -v --use-aptos2019 --use-idrid --use-messidor --warmup 20 --criterion ce -x cls_seresnext50_max_pretrain
+python train_classifier_baseline.py -m seresnext50_avg -a light -f 0 -b 60 --fp16 -o AdamW -lr 1e-4 -wd 1e-4\
+  -e 50 --warmup 10 -v --use-aptos2019 --use-idrid --use-messidor -x cls_seresnext50_avg_pretrain
 sleep 5
 
+REM Medium augmentation
+python train_classifier_baseline.py -m seresnext50_avg -a medium -f 0 -b 60 --fp16 -o AdamW -lr 5e-5 -wd 1e-4\
+  -e 50 -v --use-aptos2019 --use-idrid --use-messidor -t cls_seresnext50_avg_pretrain.pth
+sleep 5
+
+REM Hard augmentation
+python train_classifier_baseline.py -m seresnext50_avg -a hard -f 0 -b 60 --fp16 -o AdamW -lr 5e-5 -wd 1e-4\
+  -e 50 -v --use-aptos2019 --use-idrid --use-messidor -t cls_seresnext50_avg_pretrain.pth
+sleep 5
+
+REM Regression
+python train_regression_baseline.py -m seresnext50_avg -a light -f 0 -b 60 --fp16 -o AdamW -lr 1e-4 -wd 1e-4\
+  -e 50 --warmup 10 -v --use-aptos2019 --use-idrid --use-messidor -t seresnext50_avg_pretrain.pth -x reg_seresnext50_avg_pretrain
+sleep 5
+
+python train_regression_baseline.py -m seresnext50_avg -a medium -f 0 -b 60 --fp16 -o AdamW -lr 5e-5 -wd 1e-4\
+  -e 50  -v --use-aptos2019 --use-idrid --use-messidor -t reg_seresnext50_avg_pretrain.pth
+sleep 5
+
+python train_regression_baseline.py -m seresnext50_avg -a hard -f 0 -b 60 --fp16 -o AdamW -lr 5e-5 -wd 1e-4\
+  -e 50  -v --use-aptos2019 --use-idrid --use-messidor -t reg_seresnext50_avg_pretrain.pth
+sleep 5
